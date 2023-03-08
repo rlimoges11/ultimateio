e54a2a038cb3a3327bb18c6bd94f9a7a7696eca1
Rlimoges <rlimoges11@gmail.com>
2023-03-08 15:37:14 -0500

    Vue working

--- ultimate/src/App.vue
+++ ultimate/src/App.vue
@@ -1,85 +1,44 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import {RouterLink, RouterView} from 'vue-router'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="wrapper">
+    <div v-for="n in 19999" class="box"><span :data-num="n">{{ String.fromCharCode(n) }}</span></div>
+  </div>
 </template>
 
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+.box {
+  background-color: #444;
+  color: #fff;
+  font-size: 150%;
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.box:nth-child(even) {
+  background-color: #ccc;
+  color: #000;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.wrapper {
+  display: grid;
+  grid-gap: 0;
+  grid-template-columns: repeat(99, 50px);
+  grid-template-rows: repeat(99, 50px);
+  grid-auto-flow: column;
+  /*overflow: hidden;*/
+  width: 100vw;
+  height: 100vh;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.wrapper span {
+  font-size: 200%;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
+
